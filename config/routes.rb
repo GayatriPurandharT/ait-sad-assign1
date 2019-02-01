60606fc4ac66eab348cd77caa60c2c85fd389b15
Kantapon P <kantapono.pornprasertsakul@gmail.com>
2019-02-01 22:10:41 +0700

    Make Comments under Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   root :to => "posts#index"
-  resources :posts
+  resources :posts do
+    resources :comments, except: [:show, :index]
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
