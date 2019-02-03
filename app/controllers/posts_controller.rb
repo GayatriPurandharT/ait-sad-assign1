9659f76e443405e54c87b986a7a51fe31722a2b2
GayatriPurandharT <st119978@ait.asia>
2019-02-03 15:14:42 +0000

    authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-
   # GET /posts
   # GET /posts.json
   def index
@@ -27,6 +26,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user = current_user
+    @post.author = current_user.username
 
     respond_to do |format|
       if @post.save
@@ -43,6 +43,8 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1.json
   def update
     @post.user = current_user
+    @post.author = current_user.username
+    puts current_user.username
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
@@ -72,6 +74,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:content, :title, :user_id)
+      params.require(:post).permit(:content, :title, :author)
     end
 end
